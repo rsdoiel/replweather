2069f9237be1b0a1422e15171c1233aa8852a8f7
R. S. Doiel <rsdoiel@gmail.com>
2016-04-22 11:33:48 -0700

    Added two simple example repls

---
+++ cmds/rw1/rw1.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	// Standard Packages
+	"fmt"
+
+	// 3rd Party Packages
+	"github.com/robertkrimen/otto"
+
+	// Caltech Packages
+	"github.com/caltechlibrary/ostdlib"
+)
+
+func main() {
+	fmt.Println("Welcome to r1, the simplest repl built with ostdlib and otto")
+	fmt.Println("use .exit to quit the repl, .help to list the dot commands")
+	vm := otto.New()
+	js := ostdlib.New(vm)
+	js.Repl()
+}
---
+++ cmds/rw2/rw2.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	// Standard Packages
+	"fmt"
+
+	// 3rd Party Packages
+	"github.com/robertkrimen/otto"
+
+	// Caltech Packages
+	"github.com/caltechlibrary/ostdlib"
+)
+
+func main() {
+	fmt.Println("Welcome to r2 adds a basic set of objects, additional help and autocomplete")
+	fmt.Println("use .exit to quit the repl, .help to list the dot commands")
+	vm := otto.New()
+	js := ostdlib.New(vm)
+	js.AddExtensions()
+	js.AddHelp()
+	js.AddAutoComplete()
+	js.Repl()
+}
